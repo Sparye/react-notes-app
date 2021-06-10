4f2352ef21ac9b9df800f60329ad163b9cede96d
Samuel Chen <68937749+Sparye@users.noreply.github.com>
2021-06-10 23:35:22 +1200

    added useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { useState } from 'react';
 import NotesList from './components/NotesList';
+import { nanoid } from 'nanoid';
 const App = () => {
+
+    const [notes,setNotes] = useState([
+            {
+            id: nanoid(),
+            text: "This yeet is ",
+            date: "15/06/2021",
+        },
+        {
+            id: nanoid(),
+            text: "This second is ",
+            date: "14/06/2021",
+        },
+        {
+            id: nanoid(),
+            text: "This third is ",
+            date: "12/06/2021",
+        },
+        {
+            id: nanoid(),
+            text: "This forth is ",
+            date: "15/06/2021",
+        },
+    ]);
     return (
     <div className='container'>
-        <NotesList />
+        <NotesList notes={notes}/>
     </div>
     );
 }
