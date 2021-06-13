1044eb1c3e0f133fe864ee7bf121703651dc3560
Samuel Chen <68937749+Sparye@users.noreply.github.com>
2021-06-13 13:42:56 +1200

    Added local storage

--- src/App.js
+++ src/App.js
@@ -1,56 +1,82 @@
-import { useState } from 'react';
-import NotesList from './components/NotesList';
-import { nanoid } from 'nanoid';
-import  Search  from './components/Search';
+import { useEffect, useState } from "react";
+import NotesList from "./components/NotesList";
+import { nanoid } from "nanoid";
+import Search from "./components/Search";
+import Header from "./components/Header";
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This yeet is ",
+      date: "15/06/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This second is ",
+      date: "14/06/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This third is ",
+      date: "12/06/2021",
+    },
+  ]);
 
-    const [notes,setNotes] = useState([
-            {
-            id: nanoid(),
-            text: "This yeet is ",
-            date: "15/06/2021",
-        },
-        {
-            id: nanoid(),
-            text: "This second is ",
-            date: "14/06/2021",
-        },
-        {
-            id: nanoid(),
-            text: "This third is ",
-            date: "12/06/2021",
-        },
-    ]);
+  const [searchText, setSearchText] = useState("");
 
-    const [searchText, setSearchText] = useState('');
+  const [darkMode, setDarkMode] = useState(false);
 
-    const addNote = (text) => {
-        const date = new Date();
-        const newNote = {
-            id: nanoid(),
-            text: text,
-            date: date.toLocaleDateString()
-        }
-        const newNotes = [...notes, newNote];
-        setNotes(newNotes);
-    };
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
 
-    const deleteNote = (id) => {
-        const newNotes = notes.filter((note)=>note.id !== id);
-        setNotes(newNotes);
+    if (savedNotes) {
+      setNotes(savedNotes);
     }
+  }, []);
 
-    return (
-    <div className='container'>
-        <Search handleSearchNote = {setSearchText}/>
-        <NotesList 
-            notes={notes.filter((note)=> 
-                note.text.toLowerCase().includes(searchText)
-            )} 
-            handleAddNote={addNote} 
-            handleDeleteNote={deleteNote}/>
+  useEffect(() => {
+    localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
+  }, [notes]);
+
+  //   useEffect(() => {
+  //     const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
+
+  //     if (savedNotes) {
+  //       setNotes(savedNotes);
+  //     }
+  //   }, []);
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
+  return (
+    //   if darkmode is true add class name dark-mode to div
+    <div className={`${darkMode && "dark-mode"}`}>
+      <div className="container">
+        <Header handleToggleDarkMode={setDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        <NotesList
+          notes={notes.filter((note) =>
+            note.text.toLowerCase().includes(searchText)
+          )}
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
     </div>
-    );
-}
+  );
+};
 
 export default App;
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,17 +1,18 @@
-import { MdDeleteForever } from 'react-icons/md';
+import { MdDeleteForever } from "react-icons/md";
 
 const Note = ({ id, text, date, handleDeleteNote }) => {
-    return(
-        <div className='note'>
-            <p>{text}</p>
-            <div className='note-footer'>
-                <small>{date}</small>
-                <MdDeleteForever
-                    onClick={ ()=> handleDeleteNote(id)}
-                    className='delete-icon' 
-                    size='1.3em' />
-            </div>
-        </div>
-    );
+  return (
+    <div className="note">
+      <p>{text}</p>
+      <div className="note-footer">
+        <small>{date}</small>
+        <MdDeleteForever
+          onClick={() => handleDeleteNote(id)}
+          className="delete-icon"
+          size="1.3em"
+        />
+      </div>
+    </div>
+  );
 };
 export default Note;
